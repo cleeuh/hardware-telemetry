4917b7880a7ec5fa7b50a08b68971da556d35d3f
root <clee48@hawaii.edu>
2024-01-16 23:19:37 +0000

    no loop

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ def main():
     url = os.environ.get("TELEMETRY_URL", "http://0.0.0.0:12000/api/telemetry")
     token = communications.generate_idempotent_token()
 
-    while(True):
-        results = {}
-        results["cur_time"] = fetcher.Fetch.time()
-        results["uptime"]   = fetcher.Fetch.uptime()
-        results["mem"]      = fetcher.Fetch.memory()
-        results["therm"]     =  fetcher.Fetch.temperature()
-        results["cpu_util"] = fetcher.Fetch.cpu(1)
-        # print(json.dumps(results, indent=2))
-        communications.publish(url, token=token, data=results)
-        time.sleep(0.25)
+    # while(True):
+    # time.sleep(0.25)
+    results = {}
+    results["cur_time"] = fetcher.Fetch.time()
+    results["uptime"]   = fetcher.Fetch.uptime()
+    results["mem"]      = fetcher.Fetch.memory()
+    results["therm"]     =  fetcher.Fetch.temperature()
+    results["cpu_util"] = fetcher.Fetch.cpu(1)
+    communications.publish(url, token=token, data=results)
+    # print(json.dumps(results, indent=2))
 
 
 if __name__ == "__main__":
